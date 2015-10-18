5e905dbe01c056d03a2170c3014f223947ffaf0c
Chris Mutel <cmutel@gmail.com>
2015-10-18 22:36:28 +0200

    1.0.dev0

--- bw2analyzer/__init__.py
+++ bw2analyzer/__init__.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*
+__all__ = [
+    'ContributionAnalysis',
+    'DatabaseExplorer',
+    'DatabaseHealthCheck',
+    'GTManipulator',
+    'PageRank',
+    'SerializedLCAReport',
+]
+
+__version__ = (1, 0, "dev0")
+
 from .contribution import ContributionAnalysis
 from .explorer import DatabaseExplorer
 from .health_check import DatabaseHealthCheck
@@ -6,4 +17,3 @@ from .page_rank import PageRank
 from .report import SerializedLCAReport
 from .sc_graph import GTManipulator
 
-__version__ = (0, 8)

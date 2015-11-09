b0dc9d9481f70a94a043c0ac983b88ce87804362
Chris Mutel <cmutel@gmail.com>
2015-11-09 08:08:22 +0100

    0.9.dev4

--- bw2analyzer/__init__.py
+++ bw2analyzer/__init__.py
@@ -8,7 +8,7 @@ __all__ = [
     'SerializedLCAReport',
 ]
 
-__version__ = (0, 9, "dev3")
+__version__ = (0, 9, "dev4")
 
 from .contribution import ContributionAnalysis
 from .explorer import DatabaseExplorer
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bw2analyzer',
-    version="0.9.dev3",
+    version="0.9.dev4",
     packages=["bw2analyzer", "bw2analyzer.tests"],
     author="Chris Mutel",
     author_email="cmutel@gmail.com",

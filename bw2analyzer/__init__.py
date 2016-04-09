ed43caa3f1f32d6eccaf972889b7215529f30506
Chris Mutel <cmutel@gmail.com>
2016-04-09 07:44:14 +0200

    Mark current as 0.9

--- bw2analyzer/__init__.py
+++ bw2analyzer/__init__.py
@@ -8,7 +8,7 @@ __all__ = [
     'SerializedLCAReport',
 ]
 
-__version__ = (0, 9, "dev5")
+__version__ = (0, 9)
 
 from .contribution import ContributionAnalysis
 from .explorer import DatabaseExplorer

be81873eb99951cf7a4c6fb7b98386ffb3b7c870
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2021-12-01 16:41:02 +0100

    continued work on `get_labeled_inventory`: - implemented helper to bulk load metadata from databases - implemented switch to return labeled inventory data in wide or long format - implemented tests for long format

--- bw2analyzer/lci.py
+++ bw2analyzer/lci.py
@@ -1,14 +1,60 @@
 import pandas as pd
-from bw2data import get_activity
+from bw2data import Database
 from bw2calc import LCA
+from typing import List, Union
 
 
-def get_labeled_inventory(lca: LCA) -> pd.DataFrame:
+def _get_dependent_database_names(lca: LCA) -> List[str]:
+    databases = []
+    for key in lca.remapping_dicts.keys():
+        databases += [key[0] for key in lca.remapping_dicts[key].values()]
+    return pd.unique(databases)
+
+
+def _load_database_metadata(
+    lca: LCA, cols: Union[None, List[str], str] = None
+) -> pd.DataFrame:
+    # get database names
+    databases = _get_dependent_database_names(lca)
+    # load metadata and store in one dataframe
+    df = pd.DataFrame()
+    for db in databases:
+        df = df.append(pd.DataFrame(Database(db)), ignore_index=True)
+    # drop unwanted columns
+    if cols is None:
+        cols = [
+            "id",
+            "name",
+            "location",
+            "reference product",
+            "type",
+            "unit",
+            "database",
+            "categories",
+        ]
+    elif cols == "all":
+        cols = df.columns.tolist()
+    existing_cols = [c for c in cols if c in df]
+    df = df[existing_cols]
+    # set index
+    df = df.set_index("id")
+    return df
+
+
+def get_labeled_inventory(
+    lca: LCA,
+    wide_format: bool = True,
+    usecols: Union[None, List[str], str] = None,
+) -> Union[pd.DataFrame, pd.Series]:
     """
     Take an LCA's inventory matrix and labels its rows (biosphere) and columns (technosphere) with activity metadata.
 
     Args:
         * *lca* (bw2calc.LCA): LCA object whose life cycle inventory has been calculated previously.
+        * *wide_format* (bool): Whether to return a pd.DataFrame in wide format (biosphere x technosphere)
+            or a pd.Series in long format ((biosphere x technosphere) x 1)
+        * *usecols* (None, 'all' or list of column names): which metadata fields to include in the indices.
+            None means default.
 
     Returns:
         pd.DataFrame with activity information as row and column MultiIndices.
@@ -18,17 +64,63 @@ def get_labeled_inventory(lca: LCA) -> pd.DataFrame:
         lca, "inventory"
     ), "Must calculate life cycle inventory first. Please call lci()."
 
-    rows = [
-        get_activity(lca.dicts.biosphere.reversed[i]).as_dict()
-        for i in range(lca.inventory.shape[0])
-    ]
-    columns = [
-        get_activity(lca.dicts.activity.reversed[i]).as_dict()
-        for i in range(lca.inventory.shape[1])
-    ]
+    # get activity metadata
+    meta = _load_database_metadata(lca, usecols)
 
-    return pd.DataFrame(
-        data=lca.inventory.todense(),
-        index=pd.MultiIndex.from_frame(pd.DataFrame(rows)),
-        columns=pd.MultiIndex.from_frame(pd.DataFrame(columns)),
-    )
+    if wide_format:
+
+        # map local matrix indices to global database ids
+        rows_global = lca.dicts.biosphere.reversed.values()
+        cols_global = lca.dicts.activity.reversed.values()
+
+        # associate metadata, drop columns which are empty
+        row_meta = (
+            pd.DataFrame(index=rows_global, data=rows_global, columns=["id"])
+            .join(meta, how="left")
+            .dropna(how="all", axis=1)
+        )
+        col_meta = (
+            pd.DataFrame(index=cols_global, data=cols_global, columns=["id"])
+            .join(meta, how="left")
+            .dropna(how="all", axis=1)
+        )
+
+        # make dataframe with appropriate row and column indices
+        df = pd.DataFrame(
+            data=lca.inventory.todense(),
+            index=pd.MultiIndex.from_frame(row_meta),
+            columns=pd.MultiIndex.from_frame(col_meta),
+        )
+        return df
+    # return dataframe in long format
+    else:
+        rows, cols = lca.inventory.nonzero()
+
+        # map local matrix indices to global database ids
+        rows_global = [lca.dicts.biosphere.reversed[r] for r in rows]
+        cols_global = [lca.dicts.activity.reversed[c] for c in cols]
+
+        # filter metadata
+        row_meta = (
+            pd.DataFrame(index=rows_global, data=rows_global, columns=["id"])
+            .join(meta, how="left")
+            .reset_index(drop=True)
+        )
+        col_meta = (
+            pd.DataFrame(index=cols_global, data=cols_global, columns=["id"])
+            .join(meta, how="left")
+            .reset_index(drop=True)
+        )
+
+        # rename columns to avoid duplicate names for row and col
+        row_meta.columns = ["source_" + c for c in row_meta.columns]
+        col_meta.columns = ["target_" + c for c in col_meta.columns]
+
+        # combine row and column metadata, drop columns which are empty
+        index = pd.concat([row_meta, col_meta], axis=1).dropna(how="all", axis=1)
+        se = pd.Series(
+            index=pd.MultiIndex.from_frame(index),
+            data=lca.inventory.data,
+            name="value",
+        )
+        return se
--- tests/lci.py
+++ tests/lci.py
@@ -4,7 +4,7 @@ from bw2calc import LCA
 from bw2analyzer.lci import get_labeled_inventory
 import pytest
 import pandas as pd
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 
 def test_labeled_inventory_before_lci(tagged_fixture):
@@ -15,20 +15,22 @@ def test_labeled_inventory_before_lci(tagged_fixture):
         get_labeled_inventory(lca)
 
 
-def test_labeled_inventory(tagged_fixture):
+def test_labeled_inventory_wide_format(tagged_fixture):
     act = get_activity(("foreground", "fu"))
     method = list(methods)[0]
     lca = LCA({act: 1}, method)
     lca.lci()
-    df = get_labeled_inventory(lca)
+    df = get_labeled_inventory(lca, wide_format=True, usecols="all")
+
+    assert isinstance(df, pd.DataFrame)
 
     rows = [
         {
+            "id": 1,
             "name": "bad",
             "type": "emission",
             "database": "biosphere",
             "code": "bad",
-            "id": 1,
         },
         {
             "name": "worse",
@@ -39,15 +41,15 @@ def test_labeled_inventory(tagged_fixture):
         },
     ]
     columns = [
-        {"database": "background", "code": "first", "id": 3},
+        {"id": 3, "database": "background", "code": "first"},
         {"database": "background", "code": "second", "id": 4},
         {
+            "id": 5,
             "name": "functional unit",
             "tag field": "functional unit",
             "secondary tag": "X",
             "database": "foreground",
             "code": "fu",
-            "id": 5,
         },
         {
             "tag field": "A",
@@ -76,9 +78,123 @@ def test_labeled_inventory(tagged_fixture):
         [30.0, 0.0, 0.0, 5.0, 16.0, 27.0, 0.0],
         [0.0, 48.0, 0.0, 6.0, 14.0, 0.0, 44.0],
     ]
-    expected = pd.DataFrame(
-        data=data,
-        index=pd.MultiIndex.from_frame(pd.DataFrame(rows)),
-        columns=pd.MultiIndex.from_frame(pd.DataFrame(columns)),
+    expected = (
+        pd.DataFrame(
+            data=data,
+            index=pd.MultiIndex.from_frame(pd.DataFrame(rows)),
+            columns=pd.MultiIndex.from_frame(pd.DataFrame(columns)),
+        )
+        .reorder_levels(df.index.names, axis="index")  # allow different level orders
+        .reorder_levels(df.columns.names, axis="columns")
     )
     assert_frame_equal(df, expected)
+
+
+def test_labeled_inventory_long_format(tagged_fixture):
+    act = get_activity(("foreground", "fu"))
+    method = list(methods)[0]
+    lca = LCA({act: 1}, method)
+    lca.lci()
+    se = get_labeled_inventory(lca, wide_format=False, usecols="all")
+
+    index = [
+        {
+            "source_id": 1,
+            "source_code": "bad",
+            "source_database": "biosphere",
+            "source_name": "bad",
+            "source_type": "emission",
+            "target_id": 3,
+            "target_code": "first",
+            "target_database": "background",
+        },
+        {
+            "source_id": 1,
+            "source_code": "bad",
+            "source_database": "biosphere",
+            "source_name": "bad",
+            "source_type": "emission",
+            "target_id": 4,
+            "target_code": "second",
+            "target_database": "background",
+        },
+        {
+            "source_id": 1,
+            "source_code": "bad",
+            "source_database": "biosphere",
+            "source_name": "bad",
+            "source_type": "emission",
+            "target_id": 6,
+            "target_code": "i",
+            "target_database": "foreground",
+            "target_secondary tag": "X",
+            "target_tag field": "A",
+        },
+        {
+            "source_id": 1,
+            "source_code": "bad",
+            "source_database": "biosphere",
+            "source_name": "bad",
+            "source_type": "emission",
+            "target_id": 6,
+            "target_code": "i",
+            "target_database": "foreground",
+            "target_secondary tag": "X",
+            "target_tag field": "A",
+        },
+        {
+            "source_id": 2,
+            "source_code": "worse",
+            "source_database": "biosphere",
+            "source_name": "worse",
+            "source_type": "emission",
+            "target_id": 7,
+            "target_code": "ii",
+            "target_database": "foreground",
+            "target_secondary tag": "X",
+            "target_tag field": "C",
+        },
+        {
+            "source_id": 2,
+            "source_code": "worse",
+            "source_database": "biosphere",
+            "source_name": "worse",
+            "source_type": "emission",
+            "target_id": 7,
+            "target_code": "ii",
+            "target_database": "foreground",
+            "target_secondary tag": "X",
+            "target_tag field": "C",
+        },
+        {
+            "source_id": 2,
+            "source_code": "worse",
+            "source_database": "biosphere",
+            "source_name": "worse",
+            "source_type": "emission",
+            "target_id": 8,
+            "target_code": "iii",
+            "target_database": "foreground",
+        },
+        {
+            "source_id": 2,
+            "source_code": "worse",
+            "source_database": "biosphere",
+            "source_name": "worse",
+            "source_type": "emission",
+            "target_id": 9,
+            "target_code": "iv",
+            "target_database": "foreground",
+            "target_secondary tag": "Y",
+            "target_tag field": "C",
+        },
+    ]
+    data = [27., 16.,  5., 30., 44., 14.,  6., 48.]
+    expected = (
+        pd.Series(
+            data=data,
+            index=pd.MultiIndex.from_frame(pd.DataFrame(index)),
+            name='value',
+        )
+    )
+    assert_series_equal(se, expected)
